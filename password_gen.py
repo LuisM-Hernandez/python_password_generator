33553ef16bd43d3ceee59a16fdc1f6b5dc60f160
Luis <m.luishernandez@outlook.com>
2021-07-16 17:20:53 -0400

    Added the basic logic to first input question

--- password_gen.py
+++ password_gen.py
@@ -3,3 +3,13 @@ numeric_char = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 lower_char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 upper_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+password_length = int(input("How many characters you want the password to be: "))
+
+
+
+
+if password_length >= 8 and password_length <= 50:
+    print("It is working")
+else:
+    print("Password must contain min of 8 char and max of 50")
+
